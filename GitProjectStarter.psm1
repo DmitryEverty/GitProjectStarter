0c09400dd745498d7330d528569d52cb9f549c2b
DmitryEverty <evertymusic@gmail.com>
2019-07-09 13:36:11 +0300

    func for creating local pj

--- GitProjectStarter.psm1
+++ GitProjectStarter.psm1
@@ -22,6 +22,27 @@ function global:pjnew {
     code .
 }
 
+function global:pjnewLocal {
+
+    # Variables
+    $ProjectName = $args[0]
+    $ParrentDir = Get-Location
+    $PathToNewProject = -join ($ParrentDir,"\", $ProjectName)
+
+    # Text Output For Debuging
+    Write-Output "Your Project is called: $ProjectName in a folder $PathToNewProject"
+
+    # Operations
+    New-Item -ItemType Directory $PathToNewProject # Creating new folder inside
+    Set-Location -Path $PathToNewProject # Open powershell in newly Created folder
+    New-Item -Path . -Name "README.md" -ItemType "File" -Value "# $ProjectName"
+    New-Item -Path . -Name ".gitignore" -ItemType "File"
+    git init
+    git add .
+    git commit -m "Starting"
+    code .
+}
+
 function global:GitUploadThisProject {
     $ParrentDir = Get-Location
     $ThisFolderName = Get-Location | Select-Object | ForEach-Object{$_.ProviderPath.Split("\")[-1]}
