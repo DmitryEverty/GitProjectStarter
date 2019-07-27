a9be5f6ba1a2262483e0a3a4adfaca8cc926bbdb
DmitryEverty <evertymusic@gmail.com>
2019-07-27 20:06:03 +0300

    work without args

--- GitProjectStarter.psm1
+++ GitProjectStarter.psm1
@@ -29,13 +29,17 @@ function global:newLocalProject {
     # Variables
     $ProjectName = $args[0]
     $ParrentDir = Get-Location
+    if (!$ProjectName){
+        $ProjectName = Get-Location | Select-Object | ForEach-Object{$_.ProviderPath.Split("\")[-1]}
+        $PathToNewProject = -join ($ParrentDir,"\")
+    }else{
     $PathToNewProject = -join ($ParrentDir,"\", $ProjectName)
-
+    New-Item -ItemType Directory $PathToNewProject # Creating new folder inside
+    }
     # Text Output For Debuging
     Write-Output "Your Project is called: $ProjectName in a folder $PathToNewProject"
 
     # Operations
-    New-Item -ItemType Directory $PathToNewProject # Creating new folder inside
     Set-Location -Path $PathToNewProject # Open powershell in newly Created folder
     New-Item -Path . -Name "README.md" -ItemType "File" -Value "# $ProjectName"
     New-Item -Path . -Name ".gitignore" -ItemType "File"
